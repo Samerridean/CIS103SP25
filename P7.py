1608507f678a84dc05a15f29d69f5f5d0bc15605
Samerridean <scolon33@student.ccc.edu>
2025-02-12 19:54:04 -0600

    Add files via upload

---
+++ P7.py
@@ -0,0 +1,27 @@
+#Miles To Kilometers
+def MtK(M):
+    K=M*1.609344
+    return K
+def main():
+    M=float(input('Enter Miles:'))
+    Cal=MtK(M)
+    print('Kilometers:',Cal)
+main()
+#Fahrenheit to Clesius
+def FtC(F):
+    C=(F-32)*5/9
+    return C
+def main():
+    F=float(input('Enter Fahrenheit:'))
+    CalC=FtC(F)
+    print('Celsius:',CalC)
+main()
+#Pounds To Kilograms
+def PtK(P):
+    Kilo=P*0.45359237
+    return Kilo
+def main():
+    P=float(input('Enter Pounds:'))
+    CalK=PtK(P)
+    print('Kilograms:',CalK)
+main()
