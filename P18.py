5cff2e0deb5ac87b86ba34809ae2213e63a6abe6
Samerridean <scolon33@student.ccc.edu>
2025-04-23 19:39:53 -0500

    P18.py

--- P18.py
+++ P18.py
@@ -1,5 +1,5 @@
 from tkinter import *
-def kalvin(mainwin):
+def kalvin(mainwin,CAL):
     Kelvin=Label(mainwin,font=('Courier New',16),text='Kalvin:',fg='black',bg='yellow')
     Kelvin.place(x=100,y=250)
     Ktxtbox=Entry(mainwin,width=10,bg='white',fg='black',font=('Courier New',15))
@@ -15,20 +15,27 @@ def FAH(mainwin):
     Fahbox=Entry(width=10,bg='white',fg='black',font=('Courier New',15))
     Fahbox.place(x=275,y=400)
 def CALbttn(mainwin):
-    CALbtn=Button(mainwin,font=('Courier New',20),text='CAL',fg='black',bg='gold')
+    CALbtn=Button(mainwin,font=('Courier New',20),text='CAL',fg='black',
+                  bg='gold',command=lambda:CAL(mainwin,kalvin))
     CALbtn.place(x=160,y=600)
+def CAL(mainwin,kalvin):
+    KtF=kalvin-273.15
+    return
 def errbox(mainwin):
     error=Entry(mainwin,width=10,bg='white',fg='black',font=('Courier New',32))
     error.place(x=160,y=500)
+    clear=Button(mainwin,font=('Courier New',20),text='Clear',fg='Black',bg='gold')
+    clear.place(x=300,y=600)
 def main():
     mainwin=Tk()
     mainwin.geometry('600x800+600+150')
     mainwin.title('Kalvin Converter')
     mainwin.configure(bg='yellow')
     errbox(mainwin)
-    kalvin(mainwin)
+    kalvin(mainwin,CAL)
     CEL(mainwin)
     FAH(mainwin)
     CALbttn(mainwin)
     mainwin.mainloop()
 main()
+
