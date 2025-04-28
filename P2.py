caf4bd6c9eb5f03b0eb663754485a94c3200a6f6
Samerridean <scolon33@student.ccc.edu>
2025-04-28 18:57:51 -0500

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,15 @@
+import math
+print('Rectangle:')
+RW=float(input('Width:'))
+RH=float(input('Height:'))
+AR=RW*RH
+print('Area:',AR)
+print('\n','Triangle:')
+TB=float(input('Triangle Base:'))
+TH=float(input('Triangle Height:'))
+TA=1/2*TB*TH
+print('Area:',TA)
+print('\n','Circle:')
+CR=float(input('Radius:'))
+CA=3.1459*CR**2
+print('Area:',CA)
